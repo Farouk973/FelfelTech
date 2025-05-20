f149326bce90a5ac193ced20b38c41d44e281ba8
Alpha <felfelfarouk@yahoo.com>
2025-05-20 16:27:51 +0200

    download commit1

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -33,6 +33,7 @@ export const AboutSection = () => {
 
     <a
       href="CV_EN_Farouk_Felfel.pdf"
+      download="CV_EN_Farouk_Felfel.pdf"
       className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
     >
       Download CV
